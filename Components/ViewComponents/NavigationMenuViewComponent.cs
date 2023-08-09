8936a62ae818eeed7424f128ae5964209a9089b0
junior123w <113151392+junior123w@users.noreply.github.com>
2023-08-09 02:19:17 -0400

    making nav bar and crud operations

---
+++ Components/ViewComponents/NavigationMenuViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using LibraryBazzar.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryBazzar.Components.ViewComponents
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            var menuItems = new List<MenuItem> {
+                new MenuItem{Controller = "Home" , Action= "Index" , Label="Home"},
+
+                new MenuItem{Controller = "Reviews" , Action= "Index" , Label="Review" ,DropDownItems =new List<MenuItem>{
+                new MenuItem{Controller = "Reviews" , Action= "Index" , Label="List"},
+                new MenuItem{Controller = "Reviews" , Action= "Create" , Label="Create"},
+                } },
+                new MenuItem{Controller = "Books" , Action= "Index" , Label="Book" ,DropDownItems =new List<MenuItem>{
+                new MenuItem{Controller = "Books" , Action= "Index" , Label="List"},
+                new MenuItem{Controller = "Books" , Action= "Create" , Label="Create"},
+                } },
+                new MenuItem{Controller = "Home" , Action= "Privacy" , Label="Privacy"},
+            };
+            return View(menuItems);
+
+        }
+    }
+}
---
+++ Models/MenuItem.cs
@@ -0,0 +1,11 @@
+﻿namespace LibraryBazzar.Models
+{
+    public class MenuItem
+    {
+
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string Label { get; set; }
+        public List<MenuItem> DropDownItems { get; set; }
+    }
+}
